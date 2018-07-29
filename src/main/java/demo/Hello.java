8c18ed902cfaa08c938a455753a4cc96e7541b9e
freewind <nowindlee@gmail.com>
2018-07-29 15:02:34 +0800

    demo

--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -1,9 +1,22 @@
 package demo;
 
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+
+import java.io.File;
+
 public class Hello {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws GitAPIException {
+        String uri = "https://github.com/java-demos/java-hello-world-demo.git";
+        File localDir = new File("./target/local-repo/java-hello-world-demo");
+
+        Git.cloneRepository().setURI(uri)
+                .setDirectory(localDir)
+                .setCloneAllBranches(true)
+                .call();
+
+        System.out.println("check dir: " + localDir.getAbsolutePath());
     }
 
 }
